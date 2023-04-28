00d2c9c0c111f6278c4f132ad1c034b093c56c09
FurkanCan <furkancan1342@gmail.com>
2023-04-28 19:56:36 +0300

    make the start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:labirent_game/view/level_one_page.dart';
+import 'package:labirent_game/view/start_Page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LevelOnePage(),
+      home: StartPage(),
     );
   }
 }
